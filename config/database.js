50241597847a5f3df56c2fe751badde8efb525bf
Joseph Casal <Joseph Casal>
2016-04-20 17:37:58 -0400

    under development message on page

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     //localUrl: 'mongodb://localhost/meanstacktutorials'
-    localUrl: 'mongodb://joseph41:admin123@ec2-54-175-134-211.compute-1.amazonaws.com:27017/dummydb'
+    localUrl: 'mongodb://joseph41:admin123@ec2-52-201-206-5.compute-1.amazonaws.com:27017/dummydb'
 };
